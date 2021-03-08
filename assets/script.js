0d1d5ec7aa2b2a48ae1a54c0246388f6d77efd5f
Theresa Pidutti <treepidutti@gmail.com>
2021-03-08 15:58:01 -0700

    update weather

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+var api_key = "8e9b5af2936a3833e42ad16b630a03a1";
+
+var baseurl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=" + api_key;
+
+console.log(baseurl);
+
+var currentW = "api.openweathermap.org/data/2.5/weather?q={city name}&appid=${api_key}";
+
