7c36505c0f3ada0cc95a964ea5116ce3eb4a91d9
Theresa Pidutti <treepidutti@gmail.com>
2021-03-10 08:32:57 -0700

    update weather

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,30 @@
+var seekWord = document.querySelector("#searchForm");
+
+function submitSeek(event){
+    event.preventDefault();
+    var seekDisplay=document.querySelector("#name-input").nodeValue;
+    console.log(seekDisplay);
+}
+
+seekWord.addEventListener("sumbit", submitSeek)
+
 var api_key = "8e9b5af2936a3833e42ad16b630a03a1";
 
 var baseurl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=" + api_key;
 
-console.log(baseurl);
+// console.log(baseurl);
 
-var currentW = "api.openweathermap.org/data/2.5/weather?q={city name}&appid=${api_key}";
+var city = document.querySelector("#name-input");
 
+function showWeatherNow (city) { 
+    var weatherNowUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`
+    fetch(weatherNowUrl)
+    .then((date) => data.jason())
+    .then(function () {
+    console.log();
+    });
+}
+
+
+
+// var allWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${api_key}`
