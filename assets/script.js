9add0c3d8fa9e27ec01217120240eebb34122c7a
Theresa Pidutti <treepidutti@gmail.com>
2021-03-11 18:19:42 -0700

    updates weather

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ var api_key = "8e9b5af2936a3833e42ad16b630a03a1";
 var baseurl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=" + api_key;
 
 var seekWord = document.querySelector("#searchForm");
+var city = document.querySelector("#name-input").value;
 
 function submitSeek(event){
     event.preventDefault();
@@ -15,56 +16,55 @@ function submitSeek(event){
     showWeather(data);
     console.log(data);
 
-    // var lattitude = object.coord.lat;
-    // var longitude = object.coord.lon;
+    // var lattitude = coord.lat;
+    // var longitude = coord.lon;
     // var weatherUvUrl = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${api_key}`
     // fetch(weatherUvUrl)
-    // .then((data) => data.json())
+    // .then(data => data.json()
     // .then(function (uv) {
-    // showWeather(uv);
-    // console.log(uv);
+    // console.log(weatherUvUrl);
 
+    // }))
 
-    //make the boxes with our weather data
-
-// var fiveDays = document.createElement("card");
-// fiveDays.innerHTML = document.querySelector("#fiveDay").value;
-// console.log(fiveDays);
-
-    });
+   
+ 
+});
 }
-
     seekWord.addEventListener("submit", submitSeek)
 
     function showWeather (cityData) { 
-        console.log("showWeather")
+        // console.log(showWeather)
 
         var cityBtn = document.createElement("BUTTON");
+        // cityBtn.addEventListener("click", )
         cityBtn.innerHTML = document.querySelector("#name-input").value;
         document.body.append(cityBtn);
-    //    localStorage.
-      
-
+ 
         var displayOneDayWeather = document.querySelector("#displayWeather");
         var cityName = document.createElement("h1");
         cityName.innerHTML = cityData.name
         displayOneDayWeather.append(cityName)
         var todaysDate = moment().format("MMMM d, YYYY");
         displayOneDayWeather.append(todaysDate);
-        // var weatherIcon = `http://api.openweathermap.org/img/wn/10d@2x.png`;
-        // var weatherIcon = baseurl.weather.id;
-        // displayOneDayWeather.append(weatherIcon);
-
-        console.log(cityData)
-
+        console.log(cityData);
     }
+    
 
+var fiveDayWeather = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${api_key}`
+    
+    fetch(fiveDayWeather)
+    .then((data) => data.json())
+    .then(function (forecast) {
+    showWeather(forecast);
+    console.log(forecast);
+    });
 
+var fiveCardEl = document.createElement("card");
+fiveCardEl.setAttribute("src", fiveDayWeather);
+fiveDays.innerHTML = ""
+fiveDays.append(fiveCardEl)
+console.log(fiveCardEl);
 
-// 5 day forcast
-// var fiveDayWeather = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${api_key}`
-// fetch(fiveDayWeather)
-//     .then((data) => data.json())
-//     .then(function (forecast) {
-//     showWeather(forecast);
-//     console.log(forecast);
+var weatherIcon = "http://api.openweathermap.org/img/wn/" + weather[0].icon + "@2x.png"
+var weatherIcon = baseurl.weather.id;
+displayOneDayWeather.append(weatherIcon);
