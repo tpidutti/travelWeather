65b5d1c380adb72b96b9e81a0c9662041be314e0
Theresa Pidutti <treepidutti@gmail.com>
2021-05-03 10:13:16 -0600

    update functions

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,29 @@
 var api_key = "8e9b5af2936a3833e42ad16b630a03a1";
-var baseurl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=" + api_key;
+var baseurl =
+  "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=" + api_key;
 
 var seekWord = document.querySelector("#form");
 
 function submitSeek(e) {
   e.preventDefault();
   var city = document.querySelector("#city").value;
-  var weatherNowUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+  var weatherNowUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=imperial`;
 
   fetch(weatherNowUrl)
     .then((data) => data.json())
     .then(function (data) {
+    //   console.log(data);
       showWeather(data);
     });
 
-  var lat = coord.lat;
-  var lon = coord.lon;
+  var lat = data.coord.lat;
+  var lon = data.coord.lon;
   var weatherUvUrl = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${api_key}`;
 
-  fetch(weatherUvUrl).then((data) =>
-    data.json().then(function (uv) {
-      console.log(weatherUvUrl);
+  fetch(weatherUvUrl)
+  .then((data) => data.json()
+  .then(function (uv) {
+      console.log(data);
     })
   );
 }
@@ -40,9 +43,18 @@ function showWeather(cityData) {
   var todaysDate = moment().format("MMMM d, YYYY");
   displayOneDayWeather.append(todaysDate);
   var temp = document.createElement("h3");
+  temp.innerHTML = "temp" + cityData.main.temp;
   displayOneDayWeather.append(temp);
+  var humidity = document.createElement("h3");
+  humidity.innerHTML = "humidity" + cityData.main.humidity;
+  displayOneDayWeather.append(humidity);
+  var wind = document.createElement("h3");
+  wind.innerHTML = "wind speed" + cityData.wind.speed;
+  displayOneDayWeather.append(wind);
+  var UV = document.createElement("h3");
+  UV.innerHTML = "UV" + cityData.main.UV;
+  displayOneDayWeather.append(UV);
 }
-
 
 function fiveDayResult(city) {
   var fiveDayWeatherUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${api_key}`;
