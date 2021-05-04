4ae1ab543b97e53beead90a47a604b7971a3173a
Theresa Pidutti <treepidutti@gmail.com>
2021-05-03 20:10:15 -0600

    update functions

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,28 @@
 var api_key = "8e9b5af2936a3833e42ad16b630a03a1";
-var baseurl =
-  "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=" + api_key;
 
 var seekWord = document.querySelector("#form");
 
 function submitSeek(e) {
   e.preventDefault();
   var city = document.querySelector("#city").value;
-  var weatherNowUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=imperial`;
+  var weatherLocaUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=imperial`;
 
-  fetch(weatherNowUrl)
+  fetch(weatherLocaUrl)
     .then((data) => data.json())
     .then(function (data) {
-    //   console.log(data);
-      showWeather(data);
+      // console.log(data);
+      // showWeather(data);
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+      var weatherNowUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${api_key}&units=imperial`;
+
+      fetch(weatherNowUrl).then((data) =>
+        data.json().then(function (data) {
+          // console.log(data);
+          showWeather(data);
+        })
+      );
     });
-
-  var lat = data.coord.lat;
-  var lon = data.coord.lon;
-  var weatherUvUrl = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${api_key}`;
-
-  fetch(weatherUvUrl)
-  .then((data) => data.json()
-  .then(function (uv) {
-      console.log(data);
-    })
-  );
 }
 
 seekWord.addEventListener("submit", submitSeek);
@@ -38,26 +35,30 @@ function showWeather(cityData) {
 
   var displayOneDayWeather = document.querySelector("#current-city");
   var cityName = document.createElement("h1");
-  cityName.innerHTML = cityData.name;
+  cityName.innerHTML = `${city}`;
   displayOneDayWeather.append(cityName);
+  // console.log(cityName);
   var todaysDate = moment().format("MMMM d, YYYY");
   displayOneDayWeather.append(todaysDate);
+   var weatherIcon = document.createElement("img");
+  weatherIcon.setAttribute("src", `http://api.openweathermap.org/img/wn/${weather[0].icon}@2x.png`);
+    // displayOneDayWeather.append(weatherIcon);
   var temp = document.createElement("h3");
-  temp.innerHTML = "temp" + cityData.main.temp;
+  temp.innerHTML = "Temperture: " + cityData.current.temp;
   displayOneDayWeather.append(temp);
   var humidity = document.createElement("h3");
-  humidity.innerHTML = "humidity" + cityData.main.humidity;
+  humidity.innerHTML = "Humidity: " + cityData.current.humidity;
   displayOneDayWeather.append(humidity);
   var wind = document.createElement("h3");
-  wind.innerHTML = "wind speed" + cityData.wind.speed;
+  wind.innerHTML = "Wind Speed: " + cityData.current.wind_speed;
   displayOneDayWeather.append(wind);
   var UV = document.createElement("h3");
-  UV.innerHTML = "UV" + cityData.main.UV;
+  UV.innerHTML = "UV index: " + cityData.current.uvi;
   displayOneDayWeather.append(UV);
 }
 
 function fiveDayResult(city) {
-  var fiveDayWeatherUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${api_key}`;
+  var fiveDayWeatherUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${api_key}&units=imperial`;
 
   fetch(fiveDayWeatherUrl)
     .then((data) => data.json())
@@ -66,14 +67,16 @@ function fiveDayResult(city) {
       console.log(forecast);
     });
 
+  // var holdFiveDayWeather = document.querySelector("#future-searches");
   var fiveCardEl = document.createElement("card");
   fiveCardEl.setAttribute("src", fiveDayWeather);
   fiveDays.innerHTML = "";
-  fiveDays.append(fiveCardEl);
+  for (let i = 4; i < fiveDayWeather.length; i = i + 8)
+    fiveDays.append(fiveCardEl);
   console.log(fiveCardEl);
-
-  var weatherIcon =
-    "http://api.openweathermap.org/img/wn/" + weather[0].icon + "@2x.png";
-  var weatherIcon = baseurl.weather.id;
-  displayOneDayWeather.append(weatherIcon);
 }
+
+// function saveCityWeather() {
+//   localStorage.setItem(showWeather);
+// }
+// document.getElementById("#current-city").innerHTML = localStorage.saveCityWeather;
